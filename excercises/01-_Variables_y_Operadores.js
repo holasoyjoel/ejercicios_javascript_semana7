d68d0be46aa4abfa6d9603fe1fa175c0605b72c7
Correa Joel <joelcorrea.alejandro123@gmail.com>
2022-05-19 19:24:21 -0300

    ejericicio 01 resulto

---
+++ excercises/01-_Variables_y_Operadores.js
@@ -0,0 +1,20 @@
+/**
+ * a. Crear dos variables de tipo String y concatenarlas guardando 
+ *    el resultado en una 3er variable. (utilizar concat)
+ * */ 
+
+let cadena1 = 'Hola';
+let cadena2 = 'Mundo';
+let cadenasConcatenadas = cadena1.concat(' ' , cadena2);
+console.log(cadenasConcatenadas);
+
+/**
+ * b. Crear dos variables de tipo String y sumar el largo de cada 
+ * variable (cantidad de letras del string) guardando el resultado 
+ * de la suma en una 3er variable (utilizar length).
+ */
+
+let cadena3 = 'Me llamo';
+let cadena4 = 'Correa Joel';
+let largoCadenas = cadena3.length + cadena4.length;
+console.log(largoCadenas);
